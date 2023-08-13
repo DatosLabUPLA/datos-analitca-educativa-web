237403f2a1a3fc560038e9d7472ed35d87dab3e3
Ezequiel Lagos <ezequiel.lagos95@gmail.com>
2023-08-12 22:15:06 -0400

    Update: cambio de nombre a scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -71,8 +71,8 @@ window.addEventListener('DOMContentLoaded', event => {
     }
 
     // URL API
-    const base_url = 'https://api.datoseducativos.cl';
-    const projectsUrl = base_url + '/projects';
+    const baseUrl = 'https://api.datoseducativos.cl';
+    const projectsUrl = baseUrl + '/projects';
 
     // Si la url es la url base
     if (window.location.pathname == '/') {
@@ -148,7 +148,7 @@ window.addEventListener('DOMContentLoaded', event => {
         const urlParams = new URLSearchParams(window.location.search);
         const id = urlParams.get('id');
 
-        const projectsUrlId = base_url + '/projects/' + id;
+        const projectsUrlId = baseUrl + '/projects/' + id;
 
         // Procesa Proyectos
         fetch(projectsUrlId)
@@ -167,7 +167,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
             if (data.ID_proyecto != '') {
                 id_proyetco = data.ID_proyecto;
-                urlProyecto = base_url + `/project/${id_proyetco}/html`;
+                urlProyecto = baseUrl + `/project/${id_proyetco}/html`;
 
                 // Obtiene el HTML del proyecto
                 fetch(urlProyecto)
